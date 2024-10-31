9fdb0e1c77226d0d097d06442e8242601ed2f5dc
Your Name <email.com>
2024-10-31 12:30:13 +0300

    Vector(heheBANANA)

---
+++ Vector/Vector.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Cryptography;
+
+class MedianFinder
+{
+    private int[] numbers;
+
+    public MedianFinder(){
+        numbers = new int[]{};
+    }
+
+    public void AddNum(int input){
+        Array.Resize(ref numbers, numbers.Length+1);
+        numbers[numbers.Length - 1] = input;
+        Array.Sort(numbers);
+    }
+
+    public double FindMedian(){
+        var len = numbers.Length;
+        if (len%2 == 0){
+            return (Convert.ToDouble(numbers[len/2])+Convert.ToDouble(numbers[len/2-1]))/2;
+        } else{
+            return numbers[len/2];
+        }
+    }
+}
+
+class Program
+{
+    static void Main(){
+        MedianFinder medianFinder = new MedianFinder();
+        medianFinder.AddNum(1);
+        medianFinder.AddNum(3);
+        medianFinder.AddNum(4);
+        medianFinder.AddNum(5);
+        Console.WriteLine(medianFinder.FindMedian());
+    }
+}
