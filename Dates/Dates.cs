8db821b747194de3c3c45d7295f26ddebd195249
Your Name <email.com>
2024-10-31 11:05:27 +0300

    Dates(2)

--- Dates/Dates.cs
+++ Dates/Dates.cs
@@ -4,26 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-    //переменные
+        // переменные
         string[] startTimeStrings = new string[]
         {
-            "10:00",   
-            "11:00",   
-            "15:00",   
-            "15:30",   
-            "16:50"    
+            "10:00",
+            "11:00",
+            "15:00",
+            "15:30",
+            "16:50"
         };
         DateTime[] startTimes = Array.ConvertAll(startTimeStrings, time => DateTime.ParseExact(time, "HH:mm", null).Add(DateTime.Today.TimeOfDay));
-        int[] durations = new int[] { 60, 30, 10, 10, 40 };
+        int[] durations = new int[] { 60, 30, 10, 10, 50 };
         DateTime beginWorkingTime = DateTime.Today.AddHours(8);
         DateTime endWorkingTime = DateTime.Today.AddHours(18);
         int consultationTime = 30;
 
-    //вызов функции
+        // вызов функции
         string[] result = DatesBumBum(beginWorkingTime, endWorkingTime, startTimes, durations, consultationTime);
 
-    //вывод
-        var count = 0;
+        // вывод
+        int count = 0;
         foreach (string date in result)
         {
             if (!string.IsNullOrEmpty(date))
@@ -37,44 +37,48 @@ class Program
 
     static string[] DatesBumBum(DateTime beginWorkingTime, DateTime endWorkingTime, DateTime[] startTimes, int[] durations, int consultationTime)
     {
-    //переменные
+        // переменные
         int maxIntervals = (int)((endWorkingTime - beginWorkingTime).TotalMinutes / consultationTime);
         string[] availablePeriods = new string[maxIntervals];
-        int index = 0; 
-        DateTime workTime = beginWorkingTime; 
-        int i = 0; 
+        int index = 0;
+        DateTime workTime = beginWorkingTime;
+        int i = 0;
 
-        //проход по рабочему времени
+        // проход по рабочему времени
         while (workTime < endWorkingTime)
         {
             DateTime nextBusyStart = (i < startTimes.Length) ? startTimes[i] : endWorkingTime;
-            
             TimeSpan availableTime = nextBusyStart - workTime;
             int availableMinutes = (int)availableTime.TotalMinutes;
-            
-        //проход по свободному времени
+
+            // проход по свободному времени
             while (availableMinutes >= consultationTime)
             {
                 DateTime nextAvailableEnd = workTime.AddMinutes(consultationTime);
-
-                if (nextAvailableEnd > endWorkingTime){
+                
+                // Проверяем, чтобы не выходить за конец рабочего дня
+                if (nextAvailableEnd > endWorkingTime)
+                {
                     nextAvailableEnd = endWorkingTime;
                 }
-                availablePeriods[index++] = $"{workTime:HH:mm}-{nextAvailableEnd:HH:mm}"; // ●	список подходящих свободных временных промежутков (в массив строк формата HH:mm-HH:mm)
-
+                
+                availablePeriods[index++] = $"{workTime:HH:mm}-{nextAvailableEnd:HH:mm}";
+                
                 workTime = nextAvailableEnd;
-                availableMinutes -= consultationTime; 
+                availableMinutes -= consultationTime;
             }
 
-            //следующее рабочее время
+            // Переход к следующему занятому периоду
             if (i < startTimes.Length)
             {
                 workTime = startTimes[i].AddMinutes(durations[i]);
-                i++; 
+                i++;
+            }
+            else
+            {
+                break;
             }
         }
         return availablePeriods;
     }
 }
-
-
