228246b25765571c0f6169dafaf4e716bf20d7ee
Your Name <email.com>
2024-11-13 20:32:37 +0300

    Intervals

--- Mediana/obj/Debug/net8.0/Vector.AssemblyInfo.cs
+++ Mediana/obj/Debug/net8.0/Vector.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vector")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fdb0e1c77226d0d097d06442e8242601ed2f5dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+118071f527cfa83255a9a2cfd85a3a369d6c44da")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vector")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vector")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
