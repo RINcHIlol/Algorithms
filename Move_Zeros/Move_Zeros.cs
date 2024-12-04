ee4b732883d6f13de5f5db4f360ab4a84e1efe48
Your Name <email.com>
2024-12-04 11:22:49 +0300

    Replays; Move_Zeros

---
+++ Move_Zeros/Move_Zeros.cs
@@ -0,0 +1,32 @@
+﻿class Zeros
+{
+    public int[] Move(int[] input)
+    {
+        for(int i = 0; i < input.Length-1; i++)
+        {
+            if (input[i] == 0)
+            {
+                for (int k = i; k < input.Length-1; k++)
+                {
+                    input[k] = input[k+1];
+                }
+                input[input.Length-1] = 0;
+            }
+        }
+        return input;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int[] buba = {0, 2, 0, 4, 5, 0};
+        var Mover = new Zeros();
+        buba = Mover.Move(buba);
+        foreach(var i in buba)
+        {
+            Console.Write(i + " ");
+        }
+    }
+}
